1777b1686a34fc49776224a2238fda5e6668a96a
Atharva <atharvamishramain@gmail.com>
2026-09-04 00:16:46 +1000

    modified user interface class to have a different menu for the master user, modified authenticator class to hold a list of users, added functions to Authenticator to handle master user modifiying clearance level for normal users, also updated User class to allow for clearance level modification. working on AES encryption, added AES key generation functionality to the Authenticator class

--- main.py
+++ main.py
@@ -15,34 +15,43 @@ load_dotenv()
 
 class User:
 
-    def __init__(self, p_user: str, p_salt: hex, p_passwd_hash: hex, p_iteration, p_clearnace_level: int = 0):
-        self.user = p_user
-        self.salt = p_salt
-        self.passwd_hash = p_passwd_hash
-        self.iterations = p_iteration
-        self.clearance_level = p_clearnace_level
+    # Initalization
+    def __init__(self, p_user: str, p_salt: bytes, p_passwd_hash: hex, p_iteration, p_clearnace_level: int = 0):
+        self.name: str = p_user
+        self.salt: bytes = bytes.fromhex(p_salt)
+        self.passwd_hash: hex = p_passwd_hash
+        self.iterations: int = int(p_iteration)
+        self.clearance_level: int = int(p_clearnace_level)
     
     # Returns the user's clearance level
-    def get_clearance_level(self) -> int:
-        return self.clearance_level
+    def set_clearance_level(self, level: int) -> None:
+        self.clearance_level = level
 
     # Returns the user's details in a specific format
     def __str__(self):
-        return f"{self.user}|{self.salt}|{self.passwd_hash}|{self.iterations}|{self.clearance_level}\n"
+        return f"{self.name}|{self.salt.hex()}|{self.passwd_hash}|{self.iterations}|{self.clearance_level}\n"
         
 class File:
 
-    def __init__(self, name: str, cl: int) -> None:
+    meta_data: Path = os.getenv("META_DATA")
 
-        self._name = name
-        self._cl = cl
-        self._path = Path(DIRECTORY + "/" + self._name)
+    def __init__(self, p_name: str, p_clearance_level: int) -> None:
+
+        self.name = p_name
+        self.clearance_level = p_clearance_level
+        self.path = Path(os.getenv("DIRECTORY") + "/" + self.name)
 
     # Create a new file
-    def create(self,) -> bool:
+    def create(self) -> bool:
 
         try:
-            open(self._path, 'x')
+            # Create file
+            open(self.path, 'x')
+
+            # Add file meta data
+            with open(self.meta_data, "a") as file:
+                file.write(f"{self.name}|{self.clearance_level}")
+
             return True
 
         except FileExistsError as e:
@@ -54,7 +63,7 @@ class File:
     def read(self) -> None:
 
         try:
-            with open(self._path, 'r') as file:
+            with open(self.path, 'r') as file:
                 print(file.read())
 
         except FileNotFoundError as e:
@@ -62,7 +71,7 @@ class File:
 
     def write(self) -> None:
 
-        with open(self._path, "a") as file:
+        with open(self.path, "a") as file:
             data = input("[q to finish] >>> ")
 
             while (data != "q"):
@@ -73,12 +82,11 @@ class File:
 
         time.sleep(1)
 
-
     # Delets the file
     def delete(self) -> None:
 
         try:
-            os.remove(self._path)
+            os.remove(self.path)
 
         except Exception as e:
             print(f"[!] Error: {e}")
@@ -88,7 +96,9 @@ class FileManager:
     # Keeps tracks of all the files
     stored_files = []
 
-    def __init__(self):
+    # Initalizing
+    def __init__(self, p_key: bytes):
+        self.key: bytes = p_key
         self._path = Path(os.getenv("DIRECTORY"))
 
         # Checking if the folder exists
@@ -137,10 +147,11 @@ class FileManager:
             return False
         
         for idx, file in enumerate(self.stored_files):
-            print(f"[{idx+1}] {file._name}")
+            print(f"[{idx+1}] {file.name}")
 
         return True
 
+    # Encrypt a file
     def encrypt_doc(self, plaintext: bytes) -> bytes:
         iv = os.urandom(16)
         padder = PKCS7(128).padder()
@@ -151,6 +162,7 @@ class FileManager:
         doc_hash = hashlib.sha256(ciphertext).digest()
         return iv + doc_hash + ciphertext
 
+    # Decrypt a file
     def decrypt_doc(self, blob: bytes) -> bytes:
         iv, stored_hash, ciphertext = blob[:16], blob[16:48], blob[48:]
         if not hmac.compare_digest(hashlib.sha256(ciphertext).digest(), stored_hash):
@@ -165,8 +177,10 @@ class Authenticator:
 
     # Holds the current user
     user: User = None
+    users_list: list[User] = []
 
     # Default values for Salt and Hashing
+    root_user = os.getenv("MASTER_USER")
     d_bits: int = int(os.getenv("BIT_SIZE"))
     d_byte_size: int = int(os.getenv("BYTE_SIZE"))
     d_iteration: int = int(os.getenv("ITERATION"))
@@ -189,12 +203,64 @@ class Authenticator:
             self.salt = Authenticator.gen_salt()
             salt_byte = self.salt.to_bytes(self.d_byte_size, byteorder='big')
             salt_hex = salt_byte.hex()
-            self.key = Authenticator.hash_passwd(_passwd, salt_byte, self.d_iteration, self.d_byte_size)
+            self.passwd_hash = Authenticator.hash_passwd(_passwd, salt_byte, self.d_iteration, self.d_byte_size)
+
+            # AES-Key
+            kdf_salt = Authenticator.gen_salt()
+            kdf_salt_bytes = kdf_salt.to_bytes(self.d_byte_size, "big")
+            self.key = bytes.fromhex(Authenticator.hash_passwd(_passwd, kdf_salt_bytes, self.d_iteration, self.d_byte_size))
 
             # Saving Master user's details
-            data = User(_user, salt_hex, self.key, self.d_iteration, -1)
-            if (self.save(data, self.master_database, 'w')):
+            data = User(_user, salt_hex, self.passwd_hash, self.d_iteration, -1)
+            if (Authenticator.save(data, self.master_database, 'w')):
+
+                # Saving the salt for AES-Key
+                with open(Authenticator.master_database, 'a') as file:
+                    file.write(kdf_salt_bytes.hex())
                 print("[*] Master User Created!")
+
+        else:
+            _passwd = os.getenv("MASTER_PASSWORD").encode("utf-8")
+
+            with open(Authenticator.master_database, "r") as file:
+                lines = file.readlines()
+                kdf_salt_bytes = bytes.fromhex(lines[1].strip())
+                self.key = bytes.fromhex(Authenticator.hash_passwd(_passwd, kdf_salt_bytes, self.d_iteration, self.d_byte_size))
+
+
+        # Store all users in users_list
+        if (self.database.exists()):
+            with open(self.database, 'r') as file:
+                
+                for line in file:
+                    # Unpacking the list of string into arguments
+                    _user = User(*line.strip().split("|"))
+                    Authenticator.users_list.append(_user)
+
+    # List users
+    @staticmethod
+    def list_users() -> int:
+        for idx, _user in enumerate(Authenticator.users_list):
+            print(f"[{idx+1}] {_user.name.capitalize()}")
+
+        return len(Authenticator.users_list)
+
+    @staticmethod
+    def set_clearanace_level(p_user: int) -> None:
+        _user = Authenticator.users_list[p_user-1]
+        print(f"[ {_user.name.capitalize()} ]\n\nClearance Level: {_user.clearance_level}\n")
+
+        level = int((input("Set Clearance Level [0/1/2]>> ")))
+
+        while (level < 0 or level > 2):
+            level = int((input("Set Clearance Level [0/1/2]>> ")))
+
+        _user.set_clearance_level(level)
+
+        for s_user in Authenticator.users_list:
+            Authenticator.save(s_user, Authenticator.database, "w")
+
+        print("[*] Clearance Level Updated!")
 
     # Generates a random salt
     @staticmethod
@@ -207,7 +273,7 @@ class Authenticator:
             print(f"[!] Error generating salt: {e}")
             print("[*] Shuting down!")
 
-        exit(1)
+        sys.exit(1)
 
     # Geenerates a hash
     @staticmethod
@@ -228,8 +294,9 @@ class Authenticator:
     def verify(self, curr_hash: hex, stored_hash: hex) -> bool:
         return hmac.compare_digest(curr_hash, stored_hash)
 
+    @staticmethod
     # Save the user's credentials to a file
-    def save(self, user: User, path: Path, mode: str = "a") -> bool:
+    def save(user: User, path: Path, mode: str = "a") -> bool:
         try:
             with open(path, mode) as file:
                 file.write(str(user))
@@ -245,18 +312,17 @@ class Authenticator:
 
     # Retrives user's credentails and check if the password hashe matches the provided password
     def read(self, p_user: str, path: Path) -> User:
+
         try:
             with open(path, 'r') as file:
 
                 for line in file:
-                    _user, _salt, _hash, _iter, _clearance_level = line.strip().split("|")
 
-                    if (p_user == _user):
-                        _salt = bytes.fromhex(_salt)
-                        _iter = int(_iter)
-                        _clearance_level = int(_clearance_level)
+                   # Unpacking the list of strings into arguments
+                   _user = User(*line.strip().split("|"))
 
-                        return User(_user, _salt, _hash, _iter, _clearance_level)
+                   if (_user.name == p_user):
+                       return _user
 
         except Exception as e:
             print(f"[!] Error: {e}")
@@ -269,13 +335,12 @@ class Authenticator:
     def register(self, p_user: str, p_password: str) -> None:
         salt: int = Authenticator.gen_salt()
         salt_byte: bytes = salt.to_bytes(self.d_byte_size, byteorder='big')
-        salt_hex: hex = salt_byte.hex()
-
+        salt_hex: str = str(salt_byte.hex())
         passwd_hash: hex = Authenticator.hash_passwd(p_password.encode("utf-8"), salt_byte, self.d_iteration)
 
         # Storing user data
-        self.user: User = User(p_user, salt_hex, passwd_hash, self.d_iteration, 0)
-        success: bool = self.save(self.user, self.database)
+        Authenticator.user: User = User(p_user, salt_hex, passwd_hash, self.d_iteration, 0)
+        success: bool = Authenticator.save(Authenticator.user, self.database)
 
         if (not(success)):
             print("[!] Registration Failed!")
@@ -283,14 +348,18 @@ class Authenticator:
 
         print("[*] Registration Successful!")
 
-        ui: UserInterface = UserInterface()
-        ui.menu()
+        ui: UserInterface = UserInterface(self.key)
+
+        if (Authenticator.user.clearance_level < 0):
+            ui.dev_menu()
+        else:
+            ui.menu()
 
     # Authentication
     def login(self, p_user: str,  p_password: str) -> None:
 
         # Retriving user data from the database
-        data: User = self.read(p_user, self.database)
+        data: User = self.read(p_user, Authenticator.master_database if p_user == Authenticator.root_user else Authenticator.database)
 
         if (data):
 
@@ -307,20 +376,73 @@ class Authenticator:
 
             # Setting the current user
             print("[*] Authentication Successful!")
-            self.user = data
+            Authenticator.user = data
 
-        time.sleep(1)
-        ui: UserInterface = UserInterface()
-        ui.menu()
+            time.sleep(0.5)
+            ui: UserInterface = UserInterface(self.key)
+
+            if (Authenticator.user.clearance_level < 0):
+                ui.dev_menu()
+            else:
+                ui.menu()
+
+        else:
+            print("[!] User not found!")
 
 class UserInterface:
 
-    def __init__(self): 
-        self.file_manager = FileManager()
+    def __init__(self, p_key: bytes): 
+        self.file_manager = FileManager(p_key)
         self.choice = None
 
+    def dev_menu(self) -> None:
+        self.clear()
+        print(f"[1] Create a new file\n[2] Read a file\n[3] Append to a file\n[4] Delete a file\n[5] Set clearance level\n[6] Quit")
+        choice = int(input(">>> "))
+        self.clear()
+
+        match (choice):
+
+            # Create a new file
+            case 1:
+                self.create()
+                self.dev_menu()
+
+            # Read a file
+            case 2:
+                self.read()
+                self.dev_menu()
+
+            # Write to a file
+            case 3:
+                self.write()
+                self.dev_menu()
+
+            # Delete a file
+            case 4:
+                self.delete()
+                self.dev_menu()
+
+            # Set user's clearance level
+            case 5:
+                length = Authenticator.list_users()
+
+                choice = int(input(">>> "))
+                while (choice > length or choice < length):
+                    choice = int(input(">>> "))
+
+                self.clear()
+                Authenticator.set_clearanace_level(choice)
+
+                time.sleep(0.5)
+                self.dev_menu()
+
+            # Quit
+            case _:
+                return
 
     def menu(self):
+
         self.choice = None
         self.clear()
         print(f"[1] Create a new file\n[2] Read a file\n[3] Append to a file\n[4] Delete a file\n[5] Quit")
@@ -331,59 +453,22 @@ class UserInterface:
 
             # Create a new file
             case 1:
-                print("[q] to return")
-                file_name = input("Enter file name [q to return]: ")
-                
-                if (file_name != "q" and self.file_manager.create(file_name)):
-                    print("[*] File created!")
-                    time.sleep(1)
-
+                self.create()
                 self.menu()
 
             # Read a file
             case 2:
-                if (self.file_manager.list_files()):
-                    self.choice = int(input(">>> "))
-                    self.file_manager.read(self.choice)
-                    self.choice = input("[q] to return ")
-
-                else:
-                    print("[-] No files found!")
-                    time.sleep(1)
-
+                self.read()
                 self.menu()
 
             # Append to a file
             case 3:
-                if (self.file_manager.list_files()):
-                    self.choice = int(input(">>> "))
-                    self.clear()
-                    self.file_manager.write(self.choice)
-
-                else:
-                    print("[-] No files found!")
-                    time.sleep(1)
-
+                self.write()
                 self.menu()
 
             # Delete a file
             case 4:
-                if (self.file_manager.list_files()):
-                    print("[0] to return")
-                    self.choice = int(input(">>> "))
-
-                    if (self.choice == 0):
-                        self.menu()
-
-                    if (not(self.file_manager.delete(self.choice))):
-                        self.menu()
-
-                    print("[*] File Deleted!")
-
-                else:
-                    print("[-] No Files found")
-                    time.sleep(1)
-
+                self.delete()
                 self.menu()
 
             case _:
@@ -392,6 +477,55 @@ class UserInterface:
     # Clear the terminal
     def clear(self) -> None:
         print("\033[H\033[2J", end="")
+
+    # Create a new file
+    def create(self) -> None:
+        print("[q] to return")
+        file_name = input("Enter file name [q to return]: ")
+        
+        if (file_name != "q" and self.file_manager.create(file_name)):
+            print("[*] File created!")
+            time.sleep(1)
+
+    # Read a file
+    def read(self) -> None:
+        if (self.file_manager.list_files()):
+            self.choice = int(input(">>> "))
+            self.file_manager.read(self.choice)
+            self.choice = input("[q] to return ")
+
+        else:
+            print("[-] No files found!")
+            time.sleep(1)
+
+    # Write to a file
+    def write(self) -> None:
+        if (self.file_manager.list_files()):
+            self.choice = int(input(">>> "))
+            self.clear()
+            self.file_manager.write(self.choice)
+
+        else:
+            print("[-] No files found!")
+            time.sleep(1)
+
+    # Delete a file
+    def delete(self) -> None:
+        if (self.file_manager.list_files()):
+            print("[0] to return")
+            self.choice = int(input(">>> "))
+
+            if (self.choice == 0):
+                self.menu()
+
+            if (not(self.file_manager.delete(self.choice))):
+                self.menu()
+
+            print("[*] File Deleted!")
+
+        else:
+            print("[-] No Files found")
+            time.sleep(1)
 
 # Entrypoint
 def main(args):
